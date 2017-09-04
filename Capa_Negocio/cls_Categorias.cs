df5d038c86f8e854536befd8dd1376353856fbc9
Josvany_G <Josvany_G@JOSVANY>
2017-09-03 22:13:48 -0600

    Nueva Capa Neg

---
+++ Capa_Negocio/cls_Categorias.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos.Orm
+{
+    public class cls_Categorias
+    {
+        #region "Propiedades"
+
+        private Guid CatIntIdValue;
+
+        public Guid CatIntId
+        {
+            get { return CatIntIdValue; }
+            set { CatIntIdValue = value; }
+        }
+
+        private string CatNombreValue;
+
+        public string CatNombre
+        {
+            get { return CatNombreValue; }
+            set { CatNombreValue = value; }
+        }
+
+        private string CatCodigoValue;
+
+        public string CatCodigo
+        {
+            get { return CatCodigoValue; }
+            set { CatCodigoValue = value; }
+        }
+
+        private bool CatStatusValue;
+
+        public bool CatStatus
+        {
+            get { return CatStatusValue; }
+            set { CatStatusValue = value; }
+        }
+
+        #endregion
+
+
+        #region "Metodos"
+        public void Create(cls_Categorias objCat)
+        {
+            Conexion.GDatos.Ejecutar("SP_IM_Categoria",objCat.CatIntId, objCat.CatNombre, objCat.CatCodigo, objCat.CatStatus);
+        }
+
+        #endregion
+    }
+}
---
+++ DB_Farmacia/dbo/Stored Procedures/SP_ALL_CAT.sql
@@ -0,0 +1,10 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE SP_ALL_CAT
+AS
+BEGIN
+	SELECT * FROM CAT_CATEGORIA
+END
