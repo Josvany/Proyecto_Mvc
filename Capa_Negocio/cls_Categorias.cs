af716d7021ff6f8a605aeba0b991bc68a8b380dd
Josvany_G <Josvany_G@JOSVANY>
2017-09-06 22:35:08 -0600

    Probando mostra datos

--- Capa_Negocio/cls_Categorias.cs
+++ Capa_Negocio/cls_Categorias.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace Capa_Datos.Orm
 {
@@ -10,37 +13,38 @@ namespace Capa_Datos.Orm
     {
         #region "Propiedades"
 
-        private Guid CatIntIdValue;
+        public Guid CatIntIdValue { get; set; }
 
-        public Guid CatIntId
-        {
-            get { return CatIntIdValue; }
-            set { CatIntIdValue = value; }
-        }
 
-        private string CatNombreValue;
+        //public Guid CatIntId
+        //{
+        //    get { return CatIntIdValue; }
+        //    set { CatIntIdValue = value; }
+        //}
 
-        public string CatNombre
-        {
-            get { return CatNombreValue; }
-            set { CatNombreValue = value; }
-        }
+        public string CatNombreValue { get; set; }
 
-        private string CatCodigoValue;
+        //public string CatNombre
+        //{
+        //    get { return CatNombreValue; }
+        //    set { CatNombreValue = value; }
+        //}
 
-        public string CatCodigo
-        {
-            get { return CatCodigoValue; }
-            set { CatCodigoValue = value; }
-        }
+        public string CatCodigoValue { get; set; }
 
-        private bool CatStatusValue;
+        //public string CatCodigo
+        //{
+        //    get { return CatCodigoValue; }
+        //    set { CatCodigoValue = value; }
+        //}
 
-        public bool CatStatus
-        {
-            get { return CatStatusValue; }
-            set { CatStatusValue = value; }
-        }
+        public bool CatStatusValue { get; set; }
+
+        //public bool CatStatus
+        //{
+        //    get { return CatStatusValue; }
+        //    set { CatStatusValue = value; }
+        //}
 
         #endregion
 
@@ -48,7 +52,33 @@ namespace Capa_Datos.Orm
         #region "Metodos"
         public void Create(cls_Categorias objCat)
         {
-            Conexion.GDatos.Ejecutar("SP_IM_Categoria",objCat.CatIntId, objCat.CatNombre, objCat.CatCodigo, objCat.CatStatus);
+            Conexion.GDatos.Ejecutar("SP_IM_Categoria",objCat.CatIntIdValue, objCat.CatNombreValue, objCat.CatCodigoValue, objCat.CatStatusValue);
+        }
+
+
+        public  List<cls_Categorias> Listar()
+        {
+            Conexion.IniciarSesion(".","DB_Farmacia","sa","josvany");
+
+            var objCat = new List<cls_Categorias>();
+
+           var dt =  Conexion.GDatos.TraerDataTable("SP_Listar", "CAT_CATEGORIA");
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                objCat.Add(new cls_Categorias
+                {
+                    CatIntIdValue = (Guid)dt.Rows[i][0],
+                    CatNombreValue = dt.Rows[i][0].ToString()
+                });
+            }
+            //if (dt.Rows.Count > 0)
+            //{
+            //    objCat.CatIntIdValue = (Guid)dt.Rows[0][0];
+            //    objCat.CatNombreValue = dt.Rows[0][1].ToString();
+            //}
+
+            return objCat;
         }
 
         #endregion
--- Presentacion/Controllers/HomeController.cs
+++ Presentacion/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Capa_Datos;
+using Capa_Datos.Orm;
 
 namespace Presentacion.Controllers
 {
     public class HomeController : Controller
     {
+        cls_Categorias objCat = new cls_Categorias();
         public ActionResult Index()
         {
-            return View();
+            return View(objCat.Listar());
         }
 
         public ActionResult About()
