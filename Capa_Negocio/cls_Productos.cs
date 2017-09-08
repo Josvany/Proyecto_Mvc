761e9fe500b4de5f210c5887f831c69b026420fa
jeanneth.mota <jeanneth.mota@gmail.com>
2017-09-08 16:50:14 -0600

    trabaja :D

---
+++ Capa_Negocio/cls_Productos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos;
+
+namespace Capa_Negocio
+{
+    public class cls_Productos
+    {
+        #region "Propiedades"
+
+        public Guid ProdIntId { get; set; }
+
+        public string ProdNombre { get; set; }
+
+        public string ProdSysName { get; set; }
+
+        public decimal ProdPreV { get; set; }
+
+        public decimal ProdPreC { get; set; }
+
+        public decimal ProdCant { get; set; }
+
+        public Guid CatIntId { get; set; }
+
+        public Guid UseInfIntId { get; set; }
+
+
+        public bool ProdStatus { get; set; }
+
+
+        #endregion
+
+
+
+        #region "Metodos"
+
+        public List<cls_Productos> ListarProductos()
+        {
+            Conexion.IniciarSesion(@"TXNICARAGUA\SQLEXPRESS", "DB_Farmacia", "sa", "josvany");
+
+            var objPro = new List<cls_Productos>();
+
+            var dt = Conexion.GDatos.TraerDataTable("SP_Listar", "TBL_PRODUCTO");
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                objPro.Add(new cls_Productos
+                {
+                    ProdIntId = (Guid)dt.Rows[i][0],
+                    ProdNombre = dt.Rows[i][1].ToString()
+                });
+            }
+
+            return objPro;
+        }
+
+        #endregion
+    }
+}
---
+++ Capa_Negocio/cls_Roles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos;
+
+namespace Capa_Negocio
+{
+    public class cls_Roles
+    {
+        #region "propiedades"
+
+        public Guid RolIntId { get; set; }
+
+        public string RolName { get; set; }
+
+        #endregion
+
+
+        #region "Metodos"
+
+
+        public List<cls_Roles> ListarRoles()
+        {
+            Conexion.IniciarSesion(@"TXNICARAGUA\SQLEXPRESS", "DB_Farmacia", "sa", "josvany");
+
+            var objRol = new List<cls_Roles>();
+
+            var dt = Conexion.GDatos.TraerDataTable("SP_Listar", "CAT_ROLE");
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                objRol.Add(new cls_Roles
+                {
+                    RolIntId = (Guid)dt.Rows[i][0],
+                    RolName = dt.Rows[i][1].ToString()
+                });
+            }
+       
+            return objRol;
+        }
+
+        #endregion
+    }
+}
